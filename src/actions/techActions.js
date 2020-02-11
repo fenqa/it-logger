498b5e41333c0e823b4bed0a561b5cdf2c2c237c
fenqa <44468983+fenqa@users.noreply.github.com>
2020-02-11 20:02:45 +0300

    Delete techs

--- src/actions/techActions.js
+++ src/actions/techActions.js
@@ -40,6 +40,21 @@ export const addTech = tech => async dispatch => {
   }
 };
 
+// Delete techs
+export const deleteTech = id => async dispatch => {
+  try {
+    setLoading();
+
+    await fetch(`/techs/${id}`, {
+      method: 'DELETE'
+    });
+
+    dispatch({ type: DELETE_TECH, payload: id });
+  } catch (err) {
+    dispatch({ type: TECHS_ERROR, payload: err.response.statusText });
+  }
+};
+
 // Set loading true
 export const setLoading = () => {
   return {
